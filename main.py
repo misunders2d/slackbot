5d67e0864feb5207102963e0ae3a1d43c0e4d05f
MISUNDERSTOOD <2djohar@gmail.com>
2024-09-09 20:09:51 +0300

    initial working version

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+import vector_search as vs
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+APP_TOKEN = os.getenv('APP_TOKEN')
+
+# Initializes your app with your bot token and socket mode handler
+app = App(token=BOT_TOKEN)
+
+@app.message()
+def handle_message(message, say):
+    bot_user_id = app.client.auth_test()["user_id"]  # Get the bot's user ID
+    bot_name = f"<@{bot_user_id}>"
+
+    # Check if the message contains the bot's name
+    if bot_name in message['text']:
+        query = message['text']
+        vector_results = vs.vector_search(query)
+        search_results = '\n\n'.join([f"Problem: {result['problem']}, solution: {result['solution']}" for key, result in vector_results.items() if result['prob'] > 0.35])
+        response = vs.get_response(query, search_results)
+        say(response.choices[0].message.content)
+
+# Start your app
+if __name__ == "__main__":
+    SocketModeHandler(app, APP_TOKEN).start()
---
+++ vector_search.py
@@ -0,0 +1,103 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import firebase_admin
+from firebase_admin import firestore,  credentials
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+}
+cred = credentials.Certificate(firebase_credentials)
+from functools import cache
+from numpy import dot, array
+from numpy.linalg import norm
+from typing import Final
+from openai import OpenAI, NotFoundError
+from openai.types.beta.thread import Thread
+
+
+OPENAI_KEY: Final = os.getenv('KNOWLEDGE_BASE_AI_KEY')
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+client = OpenAI(api_key = OPENAI_KEY)
+
+@cache
+def fetch_data_from_db():
+    stream = db.collection('knowledge').stream()
+    all_texts = []
+    for item in stream:
+        data = item.to_dict()
+        record = {'key':item.id, 'embedding':data['embedding'],'problem':data['problem'],'solution':data['solution']}
+        all_texts.append(record)
+    return all_texts
+
+def get_embedding(text):
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=text
+    )
+    return response.data[0].embedding
+
+def cosine_similarity(query_vector, stored_vector):
+    return dot(query_vector, stored_vector) / (norm(query_vector) * norm(stored_vector))
+
+def upload_texts_to_vector(texts:list):
+    for text in texts:
+        problem, solution = text
+        embedding = get_embedding('\n\n'.join(text))
+        db.collection('knowledge').add({"problem": problem, "solution": solution, "embedding": embedding})
+        
+def delete_record_from_vector(key: str):
+    db.collection('knowledge').document(key).delete()
+    
+def modify_record_vector(key: str, text: tuple):
+    problem, solution = text
+    embedding = get_embedding('\n\n'.join(text))
+    db.collection('knowledge').document(key).update({"problem": problem, "solution": solution, "embedding": embedding})
+
+def vector_search(query:str, top_n:int=5):
+    query_embedding = get_embedding(query)
+    all_texts = fetch_data_from_db()    
+
+    distances = {}
+    for record in all_texts:
+        embedding = array(record['embedding'])
+        distance = cosine_similarity(query_embedding, embedding)
+        distances[record['key']] = {
+            'problem':record['problem'],
+            'solution':record['solution'],
+            'prob':distance} 
+
+    # Sort based on distance and take the top N
+    results = dict(sorted(distances.items(), key=lambda item: item[1]['prob'], reverse=True)[:top_n])
+    return results
+
+def get_response(query, search_results):
+    pre_prompt = f'''
+                Below is the database search for my question "{query}".
+                Please summarize and structure the search to best answer my question. If there is already a structure in the search - keep it reasonably intact.
+                Please drop irrelevant results from your summary, but make sure to keep all links, file references and tool mentions.
+                If there is a specific answer to my question in the search results - please answer it first, and then summarize the rest.
+                '''
+    response = client.chat.completions.create(
+        messages = [
+            {'role':'user','content':pre_prompt},
+            {'role':'user','content':search_results}
+            ],
+        model = 'gpt-4o-mini',
+        stream = False
+    )
+    return response
